2d0a257ad28adeec54c876589640aeee594ea5ba
FikalAhmad <fikaalm26@gmail.com>
2024-01-22 02:28:21 +0700

    chore(Todo): add function to add todo

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -5,48 +5,55 @@ import { ResizablePanel } from "@/components/ui/resizable";
 import { useState } from "react";
 
 type Values = {
-  title: string;
+  id: number;
   task: string;
-  done: string;
+  done: boolean;
 };
 
 const Todo = () => {
-  const [todos, setTodos] = useState<Values>({
-    title: "",
-    task: "",
-    done: "",
-  });
+  const [activity, setActivity] = useState<string>("");
+  const [todos, setTodos] = useState<Values[]>([]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setTodos({ ...todos, [name]: value });
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const addTodo = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(todos);
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        task: activity,
+        done: false,
+      },
+    ]);
   };
 
   return (
     <ResizablePanel defaultSize={80}>
       <h1 className="font-semibold text-3xl p-10">tes</h1>
-      <form onSubmit={handleSubmit}>
-        <Label>Add Title</Label>
-        <Input
-          type="text"
-          value={todos.title}
-          onChange={handleChange}
-          name="title"
-        />
+      <form onSubmit={addTodo}>
         <Label>Add Task</Label>
         <Input
           type="text"
-          value={todos.task}
-          onChange={handleChange}
+          value={activity}
+          onChange={(e) => setActivity(e.target.value)}
           name="task"
         />
         <Button type="submit">Save</Button>
       </form>
+      <div>
+        {todos.map((item, index) => {
+          return (
+            <div key={item.id} className="flex justify-between items-center">
+              <div>
+                {index + 1}. {item.task}
+              </div>
+              <div>
+                <Button className="mr-5">Edit</Button>
+                <Button>Hapus</Button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </ResizablePanel>
   );
 };
