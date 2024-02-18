dbee443b8e216704b0f6d19b0c4ade5380847495
FikalAhmad <fikaalm26@gmail.com>
2024-02-19 06:42:05 +0700

    feat: add dark mode and the toggle component

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,28 @@ import NotesList from "./pages/Notes";
 import Pomodoro from "./pages/Pomodoro";
 import Todo from "./pages/Todo";
 import AddNote from "./pages/Notes/AddNote";
-import ViewNote from "./pages/Notes/ViewNote";
+import { ThemeProvider } from "./components/theme-provider";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/notes" element={<Dashboard page={<NotesList />} />} />
-        <Route
-          path="/notes/:noteId"
-          element={<Dashboard page={<NotesList />} />}
-        />
-        <Route
-          path="/notes/addnote"
-          element={<Dashboard page={<AddNote />} />}
-        />
-        <Route path="/pomodoro" element={<Dashboard page={<Pomodoro />} />} />
-        <Route path="/todo" element={<Dashboard page={<Todo />} />} />
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/notes" element={<Dashboard page={<NotesList />} />} />
+          <Route
+            path="/notes/:noteId"
+            element={<Dashboard page={<NotesList />} />}
+          />
+          <Route
+            path="/notes/addnote"
+            element={<Dashboard page={<AddNote />} />}
+          />
+          <Route path="/pomodoro" element={<Dashboard page={<Pomodoro />} />} />
+          <Route path="/todo" element={<Dashboard page={<Todo />} />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
